67c42b2a8edf9d293dfa66ba829f91ffa48240d2
vijairam <vijairam20@gmail.com>
2018-07-30 00:11:59 +0300

    Speaker slot

---
+++ src/components/Speaker.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="speaker">
+    <figure>
+        <slot name = "image"></slot>
+    </figure>
+    <article>
+        <h1>{{name}}</h1>
+        <p>
+        <slot name = "content"></slot>
+        </p>
+    </article>
+    </div>
+</template>
+
+<script>
+export default {
+	name: "Speaker",
+	props: ["name"]
+};
+</script>
+
+<style>
+</style>
