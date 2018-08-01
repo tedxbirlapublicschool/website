7bbb629517583a153549e57a088876c2ea3f9fa8
vijairam <vijairam20@gmail.com>
2018-08-01 18:46:27 +0300

    component style

--- src/components/Speaker.vue
+++ src/components/Speaker.vue
@@ -20,4 +20,29 @@ export default {
 </script>
 
 <style>
+.speaker {
+	padding: 50px;
+	min-height: 100vh;
+	overflow: hidden;
+	color: black;
+	font-size: 2em;
+	font-weight: 300;
+	font-family: "Roboto";
+	margin: 0px;
+	display: grid;
+	grid-template-columns: 20% 75% auto;
+}
+p {
+	text-align: left;
+}
+h1 {
+	font-family: "Do Hyeon";
+	letter-spacing: 2px;
+	color: black;
+	font-size: 4em;
+	font-weight: bolder;
+	line-height: 1em;
+	padding: 0px;
+	margin: 0px;
+}
 </style>
